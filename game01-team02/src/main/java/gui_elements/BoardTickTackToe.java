248bbe529fb31e332b5e2e326b31b9efd824695b
Francisco Parra <franparpe@disroot.org>
2023-04-04 09:51:54 +0200

    Primer prototipo del grid

---
+++ game01-team02/src/main/java/gui_elements/BoardTickTackToe.java
@@ -0,0 +1,40 @@
+package gui_elements;
+
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+public class BoardTickTackToe extends JPanel {
+	private final static int ROWS = 3;
+	private final static int COLS = 3;
+
+	private JToggleButton[][] toggle_buttons;
+
+	public BoardTickTackToe() {
+		super(new GridLayout(ROWS, COLS));
+		this.initialize();
+	}
+
+	public BoardTickTackToe(LayoutManager arg0) {
+		super(arg0);
+		this.initialize();
+	}
+
+	public void initialize() {
+		this.toggle_buttons = new JToggleButton[BoardTickTackToe.ROWS][BoardTickTackToe.COLS];
+
+		for (int i = 0; i < toggle_buttons.length; i++) {
+			for (int j = 0; j < toggle_buttons[i].length; j++) {
+				this.toggle_buttons[i][j] = new JToggleButton();
+				this.toggle_buttons[i][j].setContentAreaFilled(false);
+				this.toggle_buttons[i][j].setOpaque(true);
+				this.toggle_buttons[i][j].setVisible(true);
+				this.add(this.toggle_buttons[i][j]);
+			}
+		}
+
+	}
+
+}
