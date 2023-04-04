9bd6199ebbb976a1c7dbd31152617d05b610da6d
Francisco Parra <franparpe@disroot.org>
2023-04-04 09:04:58 +0200

    first commit

---
+++ game01-team02/src/main/java/org/TA20/game01_team02/App.java
@@ -0,0 +1,13 @@
+package org.TA20.game01_team02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ game01-team02/src/test/java/org/TA20/game01_team02/AppTest.java
@@ -0,0 +1,20 @@
+package org.TA20.game01_team02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
