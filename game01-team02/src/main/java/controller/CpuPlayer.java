3b03c201be82c13100818b78705341ca3780f1ed
Francisco Parra <franparpe@disroot.org>
2023-04-05 10:52:56 +0200

    Implementado método que mira si ha ganado o no

--- game01-team02/src/main/java/controller/CpuPlayer.java
+++ game01-team02/src/main/java/controller/CpuPlayer.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -10,12 +11,14 @@ public class CpuPlayer {
 
 	private BoardController panel_controller;
 	private ArrayList<ToggleButtonWithId> array_of_available_buttons;
+	private ArrayList<Point> selected_points;
 	private boolean is_cpu_turn;
 	private boolean is_second_player;
 	private boolean is_first_player;
 
 	public CpuPlayer(BoardController controller, boolean is_first_player, boolean is_second_player) {
 		this.panel_controller = controller;
+		this.selected_points = new ArrayList<Point>();
 		// cpu has first turn if it's the first player
 		this.is_cpu_turn = is_first_player;
 		this.is_first_player = is_first_player;
@@ -42,8 +45,41 @@ public class CpuPlayer {
 		}
 	}
 
-	public void shiftTurn() {
+	public boolean checkWin() {
+		// check if there's a victory
+		
+		// Create a 2D boolean array to represent the Tic Tac Toe board
+		boolean[][] board = new boolean[3][3];
 
+		// Update the board with the moves of each player
+		for (Point move : this.selected_points) {
+			board[move.x][move.y] = true;
+		}
+
+		// Check rows
+		for (int i = 0; i < 3; i++) {
+			if (board[i][0] && board[i][1] && board[i][2]) {
+				return true;
+			}
+		}
+
+		// Check columns
+		for (int i = 0; i < 3; i++) {
+			if (board[0][i] && board[1][i] && board[2][i]) {
+				return true;
+			}
+		}
+
+		// Check diagonals
+		if (board[0][0] && board[1][1] && board[2][2]) {
+			return true;
+		}
+		if (board[0][2] && board[1][1] && board[2][0]) {
+			return true;
+		}
+
+		// No win
+		return false;
 	}
 
 	public void doAMove() {
@@ -52,8 +88,10 @@ public class CpuPlayer {
 		this.array_of_available_buttons.get(0).setUI(new ToggleButtonStyle(this.panel_controller.getX()));
 		this.panel_controller.toggleIsX();
 		this.array_of_available_buttons.get(0).setEnabled(false);
-		System.out.println("Hi");
-		// this.array_of_available_buttons.remove(0);
-
+		Point current_point = new Point(this.array_of_available_buttons.get(0).getId_x(),
+				this.array_of_available_buttons.get(0).getId_y());
+		System.out.println(current_point);
+		this.selected_points.add(current_point);
+		// System.out.println(this.checkWin());
 	}
 }
